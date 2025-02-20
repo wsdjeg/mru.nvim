c515a612b3bb4ae1752ed7fd6bb715b242400c6c
Eric Wong <wsdjeg@outlook.com>
2025-02-21 00:18:29 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mru.nvim
+
+## setup
+
+```lua
+require('mru').setup({
+    -- options
+})
+```
+
+default options:
+
+```lua
+{
+    enable_cache = true,
+    mru_cache_file = vim.fn.stdpath('data') .. '/nvim-mru.json'
+}
+```
+
+## Usage
+
+get mru:
+
+```
+require('mru').get()
+```
+
+clear mru:
+
+```
+require('mru').clear()
+```
--- lua/mru.lua
+++ lua/mru.lua
@@ -1,4 +1,6 @@
 local M = {}
+
+local enable_cache = true
 local mru_cache_file = vim.fn.stdpath('data') .. '/nvim-mru.json'
 local files = {}
 local function read_cache()
@@ -10,9 +12,27 @@ local function read_cache()
   end
 end
 
+local function unify_path(_path, ...)
+  local mod = select(1, ...)
+  if mod == nil then
+    mod = ':p'
+  end
+  local path = vim.fn.fnamemodify(_path, mod .. ':gs?[\\\\/]?/?')
+  if vim.fn.isdirectory(path) == 1 and string.sub(path, -1) ~= '/' then
+    return path .. '/'
+  elseif string.sub(_path, -1) == '/' and string.sub(path, -1) ~= '/' then
+    return path .. '/'
+  else
+    return path
+  end
+end
+
 read_cache()
 
 local function write_cache()
+  if not enable_cache then
+    return
+  end
   local file = io.open(mru_cache_file, 'w')
   if file then
     file:write(vim.json.encode(files))
@@ -21,26 +41,36 @@ local function write_cache()
 end
 
 function M.setup(opt)
+  opt = opt or {}
   local augroup = vim.api.nvim_create_augroup('mru.nvim', { clear = true })
+  if opt.enable_cache == false then
+    enable_cache = false
+  end
+  if opt.mru_cache_file then
+     mru_cache_file = opt.mru_cache_file
+  end
 
   local create_autocmd = vim.api.nvim_create_autocmd
   create_autocmd({ 'BufRead' }, {
     pattern = { '*' },
     group = augroup,
     callback = function(e)
-      local f = vim.fs.normalize(e.file)
+      local f = unify_path(e.file)
       if not vim.tbl_contains(files, f) then
         table.insert(files, f)
       end
-      if opt.enable_cache then
-        write_cache()
-      end
+      write_cache()
     end,
   })
 end
 
 function M.get()
   return files
+end
+
+function M.clear()
+  files = {}
+  write_cache()
 end
 
 return M
