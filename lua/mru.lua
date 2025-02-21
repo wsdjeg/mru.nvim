6decdfc57d69cd98c1f4cb51514f8bf1b5f49b72
Eric Wong <wsdjeg@outlook.com>
2025-02-21 09:29:48 +0800

    add telescope mru source

--- lua/mru.lua
+++ lua/mru.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local is_win = vim.fn.has('win32') == 1
-
 local enable_cache = true
 local mru_cache_file = vim.fn.stdpath('data') .. '/nvim-mru.json'
 local files = {}
@@ -14,26 +12,7 @@ local function read_cache()
   end
 end
 
-local function unify_path(_path, ...)
-  local mod = select(1, ...)
-  if mod == nil then
-    mod = ':p'
-  end
-  local path = vim.fn.fnamemodify(_path, mod .. ':gs?[\\\\/]?/?')
-  if is_win then
-    local re = vim.regex('^[a-zA-Z]:/')
-    if re:match_str(path) then
-      path = string.upper(string.sub(path, 1, 1)) .. string.sub(path, 2)
-    end
-  end
-  if vim.fn.isdirectory(path) == 1 and string.sub(path, -1) ~= '/' then
-    return path .. '/'
-  elseif string.sub(_path, -1) == '/' and string.sub(path, -1) ~= '/' then
-    return path .. '/'
-  else
-    return path
-  end
-end
+local unify_path = require('mru.utils').unify_path
 
 read_cache()
 
---
+++ lua/telescope/_extensions/mru.lua
@@ -0,0 +1,53 @@
+--=============================================================================
+-- neomru.lua --- telescope neomru extension
+-- Copyright (c) 2024 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- License: GPLv3
+--=============================================================================
+
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
+local conf = require("telescope.config").values
+local entry_display = require("telescope.pickers.entry_display")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local mru = require('mru')
+
+local function prepare_output_table()
+    local lines = vim.deep_copy(mru.get())
+
+    return lines
+end
+
+local function show_script_names(opts)
+    opts = opts or {}
+    pickers.new(opts, {
+        prompt_title = "Recent Files",
+        finder = finders.new_table {
+            results = prepare_output_table()
+        },
+        sorter = conf.generic_sorter(opts),
+        previewer = conf.file_previewer({}),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local entry = action_state.get_selected_entry()
+                -- print(vim.inspect(selection))
+                -- vim.cmd("e " + entry.value)
+                vim.cmd('e ' .. entry.value)
+            end)
+            return true
+        end,
+    }):find()
+end
+
+local function run()
+    show_script_names()
+end
+
+return require("telescope").register_extension({
+    exports = {
+        -- Default when to argument is given, i.e. :Telescope scriptnames
+        mru = run,
+    },
+})
