abb917699f9980b87001b5173ab9c7e533938e93
Eric Wong <wsdjeg@outlook.com>
2025-02-21 09:21:58 +0800

    upper disk

--- lua/mru.lua
+++ lua/mru.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local is_win = vim.fn.has('win32') == 1
+
 local enable_cache = true
 local mru_cache_file = vim.fn.stdpath('data') .. '/nvim-mru.json'
 local files = {}
@@ -18,6 +20,12 @@ local function unify_path(_path, ...)
     mod = ':p'
   end
   local path = vim.fn.fnamemodify(_path, mod .. ':gs?[\\\\/]?/?')
+  if is_win then
+    local re = vim.regex('^[a-zA-Z]:/')
+    if re:match_str(path) then
+      path = string.upper(string.sub(path, 1, 1)) .. string.sub(path, 2)
+    end
+  end
   if vim.fn.isdirectory(path) == 1 and string.sub(path, -1) ~= '/' then
     return path .. '/'
   elseif string.sub(_path, -1) == '/' and string.sub(path, -1) ~= '/' then
@@ -47,7 +55,7 @@ function M.setup(opt)
     enable_cache = false
   end
   if opt.mru_cache_file then
-     mru_cache_file = opt.mru_cache_file
+    mru_cache_file = opt.mru_cache_file
   end
 
   local create_autocmd = vim.api.nvim_create_autocmd
